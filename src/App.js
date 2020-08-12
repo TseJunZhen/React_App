aee27291d26d4995fbb6ae29c80c2fe1d7562090
tsejunzhen <tsejunzhen95@hotmail.com>
2020-08-12 17:56:11 +0800

    added: update,put,deleted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-    
-      <div className="container">
-        <ListEmployeeComponent/>
-      </div>
-
-      <FooterComponent/>
+      <Router>
+          <HeaderComponent/>
+          <div className="container">
+              <Switch>
+                <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                <Route path = "/add-employee" component = {CreateEmployeeComponent}></Route>
+                {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+                
+                <ListEmployeeComponent/>
+              </Switch>
+          </div>
+          <FooterComponent/>
+      </Router>
     </div>
   );
 }
--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -8,16 +8,37 @@ class ListEmployeeComponent extends Component {
         this.state = {
             employees: []
         }
+        this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
     }
     componentDidMount(){
         EmployeeService.getEmployees().then((res) => {
             this.setState({employees: res.data});
         });
     }
+
+    addEmployee(){
+        this.props.history.push('/add-employee');
+    }
+
+    editEmployee(id){
+        this.props.history.push(`/update-employee/${id}`);
+    }
+    deleteEmployee(id){
+        EmployeeService.deleteEmployee(id).then( res => {
+            this.setState({employees: this.state.employees.filter(employee => employee.id !== id)});
+        })
+        alert('success Deleted');
+    }
+
     render() {
         return (
             <div>
                 <h2 className="text-center">Employees List</h2>
+                <div className="row">
+                    <button className="btn btn-primary" onClick={this.addEmployee}>Add Employee</button>
+                </div>
                 <div className = "row">
                     <table className="table table-striped table-borded">
 
@@ -38,6 +59,10 @@ class ListEmployeeComponent extends Component {
                                         <td> {employee.firstName} </td>
                                         <td> {employee.lastName} </td>
                                         <td> {employee.emailId} </td>
+                                        <td> 
+                                            <button onClick = { () => this.editEmployee(employee.id)} className="btn btn-info">Update</button>
+                                            <button style={{marginLeft: "10px"}} onClick = { () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete</button>
+                                        </td>
                                     </tr>
                                 )
                             }
